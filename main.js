08396131e248b0d29175cd5c89baaad42c9ad8dd
Crudu Alex <alexcruducode@gmail.com>
2022-10-28 02:41:25 +0300

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const toggleMenu = document.getElementById("toggle");
+const hamburger = document.getElementById("hamburger");
+
+const hamburgerIcon = document.getElementById("hamburger_icon");
+const xIcon = document.getElementById("x_icon");
+
+window.onresize = function() {
+  if (window.innerWidth <= 950) toggleMenu.classList.add("hide");
+  else toggleMenu.classList.remove("hide");
+};
+
+hamburger.addEventListener("click", () => {
+  toggleMenu.classList.toggle("hide");
+  if (hamburgerIcon.className == "show") {
+    hamburgerIcon.className = "hide";
+    xIcon.className = "show";
+  } else if (hamburgerIcon.className == "hide") {
+    hamburgerIcon.className = "show";
+    xIcon.className = "hide";
+  }
+});
